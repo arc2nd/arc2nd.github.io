bc594a7cd3d477ee7b353ed420e1c97b269f2038
James Parks <arcsecond@gmail.com>
2018-07-19 12:59:09 -0700

    responsive media query css update, lecture notes, webServer.py

---
+++ webServer.py
@@ -0,0 +1,11 @@
+import os
+import BaseHTTPServer
+import CGIHTTPServer
+
+serverAddr = ("", 8000)
+
+serv = BaseHTTPServer.HTTPServer(serverAddr, CGIHTTPServer.CGIHTTPRequestHandler)
+
+serv.serve_forever()
+
+
