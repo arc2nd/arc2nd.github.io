b40b3045ee3cf84c3098586810efff3c1c5a37fc
James Parks <arcsecond@gmail.com>
2018-08-04 10:14:15 -0700

    changed product text color, rounded employee photos

--- webServer.py
+++ webServer.py
@@ -3,9 +3,7 @@ import BaseHTTPServer
 import CGIHTTPServer
 
 serverAddr = ("", 8000)
-
 serv = BaseHTTPServer.HTTPServer(serverAddr, CGIHTTPServer.CGIHTTPRequestHandler)
-
 serv.serve_forever()
 
 
